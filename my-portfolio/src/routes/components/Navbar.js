eb6290b67a0eba9643d26dca3ec3084eb88fbe8f
Arpita Bais <arpitarajput547@gmail.com>
2025-01-30 15:14:35 +0530

    Done with Homepage and nav bar

---
+++ my-portfolio/src/routes/components/Navbar.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import "./NavBarstyle.css"
+import { FaBars, FaTimes } from "react-icons/fa"
+import { useState } from 'react'
+
+const Navbar = () => {
+  const [click, setclick] = useState(false);
+  const handleclick = () => setclick(!click);
+
+  const [color, setcolor] = useState(false);
+  const changecolor = () => {
+    if (window.scrollY >= 100){
+      setcolor(true);
+    } 
+    else {
+      setcolor(false)
+    }
+  }
+
+  window.addEventListener("scroll", changecolor)
+
+  return (
+    <div className={color ? " header header-bg " : "header"}>
+      <Link to={"/"}>Portfolio</Link>
+      <ul className={click ? "nav-menu active" : "nav-menu"}>
+        <li>
+          <Link to={"/"}>Home</Link>
+        </li>
+        <li>
+          <Link to={"/projects"}>Projects</Link>
+        </li>
+        <li>
+          <Link to={"/about"}>About</Link>
+        </li>
+        <li>
+          <Link to={"/contact"}>Contact</Link>
+        </li>
+        <div className='hamburger' onClick={handleclick}>
+          {click ?
+            (<FaTimes size={20} style={{ color: "#fff" }} />)
+            :
+            (<FaBars size={20} style={{ color: "#fff" }} />)
+          }
+
+
+        </div>
+      </ul>
+    </div>
+  )
+}
+
+
+export default Navbar
